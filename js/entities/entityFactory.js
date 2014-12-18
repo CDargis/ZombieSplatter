ac50c6b32b3b93441a72a0c69f60d53eeeaf2acc
Chris Dargis <dargis.chris@gmail.com>
2014-12-18 06:33:17 -0600

    fixing up pictures

--- js/entities/entityFactory.js
+++ js/entities/entityFactory.js
@@ -1,6 +1,6 @@
 define(['entities/zombie', 'lib/easeljs', 'lib/preloadjs', 'lib/tweenjs'], function(zombieEntity) {
 
-	var baseEntity = function(type) {
+	var baseEntityModel = function(type) {
 		return {
 			type: type,
 			dead: false,
@@ -10,7 +10,7 @@ define(['entities/zombie', 'lib/easeljs', 'lib/preloadjs', 'lib/tweenjs'], funct
 
 	var decoratorFactory = { 'zombie': zombieEntity }
 	var createEntity = function(entityModel) {
-		var entity = baseEntity(entityModel.type);
+		var entity = baseEntityModel(entityModel.type);
 		decoratorFactory[entityModel.type].decorate(entity, entityModel.data);
 		return entity;
 	}
