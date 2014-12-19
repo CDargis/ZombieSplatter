405ecd3bbc66cfd161b28a45fdc2b9e98499ab58
Chris Dargis <dargis.chris@gmail.com>
2014-12-19 06:43:51 -0600

    adding load manifest

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 require(['entities/entityFactory', 'engine/gameEngine', 'lib/easeljs'], function(entityFactory, gEngine) {
+	// Load the assets
+	var loader;
+	var onLoadComplete = function(event) {
+		console.log(loader.getResult('zombieOne'));
+	}
+	loader = new createjs.LoadQueue(false);
+	loader.addEventListener('complete', onLoadComplete);
+	loader.loadManifest({src: "../assets/manifest.json", callback: "loadAssets", type: "manifest"}, true);
+
   var stage = new createjs.Stage('gameCanvas');
   gEngine.init(stage);
 });
