0511ad1d3f1cba6dba62105ce232e59326ca4954
Chris Dargis <dargis.chris@gmail.com>
2014-12-26 15:32:16 -0600

    Adding sinonJS for mocks and spies

--- src/js/sprites/zombieOne.test.js
+++ src/js/sprites/zombieOne.test.js
@@ -1,6 +1,7 @@
 define(['sprites/zombieOne'], function(zombieOne) {
 	console.log(zombieOne);
   test('sprites/zombieOne test', function () {
+  	var spy1 = this.spy();
     expect(2);
     strictEqual('Can set title', 'Can set title', 'Can set title');
     strictEqual('Can set title', 'Can set title', 'Can set title');
